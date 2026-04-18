521d47d234c9d306cf7e7180ab7b16f8ae76bb76
xp <xuanphuc12062002@gmail.com>
2026-04-18 20:37:53 +0700

    fix cors

--- src/config/allowOrigins.ts
+++ src/config/allowOrigins.ts
@@ -1,30 +1,20 @@
 export interface TAllowOrigins {
-    protocol: ('http' | 'https')[];
-    domain: string;
-    ports: number[];
+  protocol: ("http" | "https")[];
+  domain: string;
+  ports: number[];
 }
 
 const allowOrigins: TAllowOrigins[] = [
-    {
-        protocol: ['http', 'https'],
-        domain: '127.0.0.1',
-        ports: [3000],
-    },
-    {
-        protocol: ['http', 'https'],
-        domain: 'localhost',
-        ports: [3000],
-    },
-    {
-        protocol: ['http', 'https'],
-        domain: 'office.congtycaphe15.com',
-        ports: [80, 443],
-    },
-    {
-        protocol: ['http', 'https'],
-        domain: 'cf15office.checkee.vn',
-        ports: [80, 443]
-    }
+  {
+    protocol: ["http", "https"],
+    domain: "127.0.0.1",
+    ports: [5173]
+  },
+  {
+    protocol: ["http", "https"],
+    domain: "localhost",
+    ports: [5173]
+  }
 ];
 
 export default allowOrigins;
