706683ced0f2fb27408e171e751b55095dfedf05
xp <xuanphuc12062002@gmail.com>
2026-04-18 19:49:38 +0700

    fix to build

--- src/utils/make/schema.ts
+++ src/utils/make/schema.ts
@@ -1,41 +1,40 @@
-import mongoose from 'mongoose';
-import { IModificationDetails } from '../../core/types/db-types';
-import ModificationDetailsSchema from '@/database/common/ModificationDetailsSchema';
+import mongoose from "mongoose";
+import { IModificationDetails } from "../../core/types/db-types";
 
 const schema = <T>(_schema: T): T => {
-    const schema = _schema as unknown as {
-        methods: {
-            addModificationDetail: (v: Omit<IModificationDetails, 'modifiedAt'>) => void;
-        };
+  const schema = _schema as unknown as {
+    methods: {
+      addModificationDetail: (v: Omit<IModificationDetails, "modifiedAt">) => void;
     };
+  };
 
-    schema.methods.addModificationDetail = function (
-        this: { modificationDetails: IModificationDetails[] },
-        v: Omit<IModificationDetails, 'modifiedAt'>,
-    ) {
-        if (!Array.isArray(this.modificationDetails)) {
-            this.modificationDetails = [
-                {
-                    modifiedBy: v.modifiedBy,
-                    modifiedAt: new Date(),
-                    description: v.description,
-                },
-            ];
-            return;
+  schema.methods.addModificationDetail = function (
+    this: { modificationDetails: IModificationDetails[] },
+    v: Omit<IModificationDetails, "modifiedAt">
+  ) {
+    if (!Array.isArray(this.modificationDetails)) {
+      this.modificationDetails = [
+        {
+          modifiedBy: v.modifiedBy,
+          modifiedAt: new Date(),
+          description: v.description
         }
+      ];
+      return;
+    }
 
-        this.modificationDetails = [
-            ...this.modificationDetails,
-            {
-                modifiedBy: v.modifiedBy,
-                modifiedAt: new Date(),
-                description: v.description,
-            },
-        ];
-        return;
-    };
+    this.modificationDetails = [
+      ...this.modificationDetails,
+      {
+        modifiedBy: v.modifiedBy,
+        modifiedAt: new Date(),
+        description: v.description
+      }
+    ];
+    return;
+  };
 
-    return _schema as T;
+  return _schema as T;
 };
 
 export default schema;
